79a02fc0b8e5345a2784a385079a1d996e73d00c
Federico <federicompalacios@hotmail.com>
2017-11-22 18:02:44 -0300

    image, mixins, media, clean gulp code

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+// Paquete especifico de browser-sync
+browserSync = require('browser-sync').create();
+
+gulp.task('html', function(){
+   console.log("HTML"); 
+});
+
+gulp.task('watch', function(){
+   
+    watch("./app/assets/styles/**/*.css", function(){
+       gulp.start('styles'); 
+    });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,4 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-// Paquete especifico de browser-sync
-browserSync = require('browser-sync').create();
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function(){
-   console.log("de one"); 
-});
 
-gulp.task('html', function(){
-   console.log("second task"); 
-});
-
-gulp.task('styles', function(){
-    return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
-        .pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch', function(){
-    
-    watch('./app/index.html', function(){
-       gulp.start('html');
-    });
-    
-    watch("./app/assets/styles/**/*.css", function(){
-       gulp.start('styles'); 
-    });
-});
