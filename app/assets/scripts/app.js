529735926640ba805121d6a0fcc56d8ab26a4620
Federico <federicompalacios@hotmail.com>
2017-11-27 00:31:58 -0300

    Completed reveal on scroll

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/revealOnScroll';
+import $ from 'jquery';
 
 var mobile = new MobileMenu();
+new RevealOnScroll($('.feature-item'), '85%');
+new RevealOnScroll($('.testimonial'), '85%');
