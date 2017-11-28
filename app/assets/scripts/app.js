69ca2ee01e301760ac8e70895caf6d087204fbe5
Federico <federicompalacios@hotmail.com>
2017-11-27 23:38:16 -0300

    Completed modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/revealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
 var mobile = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '85%');
 var sticky = new StickyHeader();
+var modal = new Modal();
